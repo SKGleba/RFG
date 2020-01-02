d52ad40a8b9d7ddeb2302d0eb618b51005f02e29
SKGleba <30833773+SKGleba@users.noreply.github.com>
2020-01-03 00:37:21 +0100

    Update q.c

--- MGR-pc/q.c
+++ MGR-pc/q.c
@@ -2,28 +2,28 @@
     RFGMGR by SKGleba
     All Rights Reserved
 */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
 #include <stdint.h>
 
-int main(int argc, char **argv){
-	char cbuff[128], statbuf[20], etrbuf[20];
-	int sz = 0;
-	if(argc < 2) {
-		return -1;
-	}
-	if (strcmp("git", argv[1]) == 0) {
-		system("cd RFG && git add db.crypt14 && git add sr.crypt14 && git commit -m \"AutoUpdate\" && git push");
-		return 1;
-	}
-		
-	sprintf(cbuff, "curl %s |head -c 14000 > temp.html", argv[1]);
-	system(cbuff);
+int getEntry(uint8_t mode) {
+
+	int sz = 0, off = 0;
+	char statbuf[20], etr1buf[20], etr2buf[20], etr3buf[20], etr4buf[20], tempbuf[9];
+	
 	system("grep -oP '(?<=status_id=).*' temp.html > temp.status");
-	system("grep -oP '(?<=https://twitter.com/i/videos/).*' temp.html > temp.vid");
-	system("grep -oP '(?<=https://pbs.twimg.com/media/).*' temp.html > temp.img");\
+	system("grep -oP '(?<=https://twitter.com/i/videos/).*' temp.html > temp.tvid");
+	system("grep -oP '(?<=<meta  property=\"og:image\" content=\"https://pbs.twimg.com/media/).*' temp.html > temp.timg");
+	
+	memset(statbuf, 0, 20);
+	memset(etr1buf, 0, 20);
+	memset(etr2buf, 0, 20);
+	memset(etr3buf, 0, 20);
+	memset(etr4buf, 0, 20);
+	
 	FILE *fp = fopen("temp.status", "rb");
 	if (fp == NULL)
 		return 0;
@@ -33,11 +33,13 @@ int main(int argc, char **argv){
 	if (sz < 19) {
 		fclose(fp);
 		return 0;
+	} else {
+		fread(statbuf, 19, 1, fp);
+		fclose(fp);
+		printf("OP: %s, mode: %d\n", statbuf, mode);
 	}
-	fread(statbuf, 19, 1, fp);
-	fclose(fp);
 	
-	fp = fopen("temp.vid", "rb");
+	fp = fopen("temp.tvid", "rb");
 	if (fp == NULL)
 		return 0;
 	fseek(fp, 0L, SEEK_END);
@@ -47,12 +49,13 @@ int main(int argc, char **argv){
 		fclose(fp);
 		sz = 0;
 	} else {
-		fread(etrbuf, 19, 1, fp);
+		fread(etr1buf, 19, 1, fp);
 		fclose(fp);
+		printf("got %s [vid]\n", etr1buf);
 	}
 	
 	if (sz == 0) {
-		fp = fopen("temp.img", "rb");
+		fp = fopen("temp.timg", "rb");
 		if (fp == NULL)
 			return 0;
 		fseek(fp, 0L, SEEK_END);
@@ -62,29 +65,93 @@ int main(int argc, char **argv){
 			fclose(fp);
 			return 0;
 		}
-		fread(etrbuf, 19, 1, fp);
+		fread(etr1buf, 19, 1, fp);
+		printf("got %s\n", etr1buf);
+		off = ftell(fp) + 9;
+		if ((off + 19) < sz) {
+			fseek(fp, off, SEEK_SET);
+			fread(etr2buf, 19, 1, fp);
+			printf("got %s\n", etr2buf);
+			off = ftell(fp) + 9;
+			if ((off + 19) < sz) {
+				fseek(fp, off, SEEK_SET);
+				fread(etr3buf, 19, 1, fp);
+				printf("got %s\n", etr3buf);
+				off = ftell(fp) + 9;
+				if ((off + 19) < sz) {
+					fseek(fp, off, SEEK_SET);
+					fread(etr4buf, 19, 1, fp);
+					printf("got %s\n", etr4buf);
+				}
+			}
+		}	
 		fclose(fp);
 	}
 	
-	if (statbuf[0] == 0 || etrbuf[0] == 0)
+	etr1buf[19] = mode;
+	etr2buf[19] = mode;
+	etr3buf[19] = mode;
+	etr4buf[19] = mode;
+	statbuf[19] = 0x69;
+	
+	if (statbuf[0] == 0 || etr1buf[0] == 0)
 		return 0;
 	
-	statbuf[19] = 0x69;
-	etrbuf[19] = (argc > 2) ? 0x34 : 0x69;
-	
-	fp = fopen("RFG/sr.crypt14", "ab");
+	printf("writing sources...\n");
+	fp = fopen("RFG/dbs/sr.crypt15", "ab");
 	if (fp == NULL)
 		return 0;
 	fseek(fp, 0L, SEEK_END);
 	fwrite(statbuf, 20, 1, fp);
+	if (etr2buf[0] > 0) {
+		fwrite(statbuf, 20, 1, fp);
+		if (etr3buf[0] > 0) {
+			fwrite(statbuf, 20, 1, fp);
+			if (etr4buf[0] > 0) {
+				fwrite(statbuf, 20, 1, fp);
+			}
+		}
+	}
 	fclose(fp);
 	
-	fp = fopen("RFG/db.crypt14", "ab");
+	printf("writing content...\n");
+	fp = fopen("RFG/dbs/db.crypt15", "ab");
 	if (fp == NULL)
 		return 0;
 	fseek(fp, 0L, SEEK_END);
-	fwrite(etrbuf, 20, 1, fp);
+	fwrite(etr1buf, 20, 1, fp);
+	if (etr2buf[0] > 0) {
+		fwrite(etr2buf, 20, 1, fp);
+		if (etr3buf[0] > 0) {
+			fwrite(etr3buf, 20, 1, fp);
+			if (etr4buf[0] > 0) {
+				fwrite(etr4buf, 20, 1, fp);
+			}
+		}
+	}
 	fclose(fp);
+	
+	printf("all done...\n");
+	
+	return 1;
+}
+
+int main(int argc, char **argv){
+	char cbuff[128];
+	int sz = 0;
+	
+	if(argc < 2) {
+		return -1;
+	}
+	
+	if (strcmp("git", argv[1]) == 0) {
+		system("cd RFG && git add dbs/db.crypt15 && git add dbs/sr.crypt15 && git commit -m \"AutoUpdate\" && git push");
+		return 1;
+	} else {
+		sprintf(cbuff, "curl %s |head -c 14000 > temp.html", argv[1]);
+		system(cbuff);
+		return getEntry((argc > 2) ? 0x34 : 0x69);
+	}
 	
 	return 1;
 }
